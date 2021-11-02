8b27e9beca1880c92d8984886703ad33b5f519fe
MartHerr <85181673+MartHerr@users.noreply.github.com>
2021-11-02 23:12:46 +0100

    Third commit

--- src/main/java/it/test/spring/testfabrick/controller/ControllerApi.java
+++ src/main/java/it/test/spring/testfabrick/controller/ControllerApi.java
@@ -1,42 +1,49 @@
 package it.test.spring.testfabrick.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.ui.Model;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import java.net.MalformedURLException;
 
-import java.util.Arrays;
-import java.util.List;
 
 @RestController
 public class ControllerApi {
 
-    @Autowired
-    private RestTemplate restTemplate;
-
-    private static String url = "https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts";
-
-    @Value("${api.key}")
-    private String apiKey;
-
-    @Value("${Auth-Schema}")
-    private String authSchema;
-
-    @GetMapping("/allAccounts")
-    public List<Object> getAccounts(){
-        Object[] accounts = restTemplate.getForObject(url, Object[].class);
-        return Arrays.asList(accounts);
-
+    /*@Autowired
+    private RestTemplate restTemplate;*/
+    
+    
+    @GetMapping("/account")
+    public String getAccount() throws MalformedURLException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Auth-Schema","S2S");
+        headers.add("Api-Key" ,"FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP");
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        RestTemplate result = new RestTemplate();
+        ResponseEntity<String> response = result.exchange("https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts/14537780/balance/", HttpMethod.GET ,entity , String.class );
+        return response.toString();
     }
-    @GetMapping("/balance")
+
+    @GetMapping("/transiction")
+    public String getTransiction() throws MalformedURLException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Auth-Schema","S2S");
+        headers.add("Api-Key" ,"FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP");
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        RestTemplate result = new RestTemplate();
+        ResponseEntity<String> response = result.exchange("https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts/14537780/transactions?fromAccountingDate=2019-01-01&toAccountingDate=2019-12-01", HttpMethod.GET ,entity , String.class );
+        return response.toString();
+    }
+    /*@GetMapping("/balance")
     private String getHelloClient(){
-        String uri = "https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts/14537780/balance";
+        String uri = "https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts/14537780/balance/";
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri + "?Auth-Schema=" + authSchema, String.class);
         return result;
     }
+    */
 }
